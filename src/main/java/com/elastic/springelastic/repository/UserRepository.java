cc5cf672e6da009b1b29f76b1e6274795ddc120d
Divija Palleti <IHM2016005@iiita.ac.in>
2021-03-19 11:27:37 +0530

    minor changes

--- src/main/java/com/elastic/springelastic/repository/UserRepository.java
+++ src/main/java/com/elastic/springelastic/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.elastic.springelastic.repository;
 
 import com.elastic.springelastic.model.Users;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface UserRepository extends ElasticsearchRepository<Users, String> {
     List<Users> findByName(String name);
 }
